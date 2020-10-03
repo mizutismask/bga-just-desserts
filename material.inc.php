e8c0b5d3a56ceac0ec8492d5a56d5932745396d1
Séverine <severinek@gmail.com>
2020-10-03 09:33:05 +0200

    some cards added to make setup

--- material.inc.php
+++ material.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
@@ -31,7 +32,114 @@ $this->card_types = array(
 );
 
 */
+$BLUE = "blue";
+$GREEN = "green";
+$RED = "red";
+$PURPLE = "purple";
+$ORANGE = "orange";
+$YELLOW = "yellow";
+$this->colors = [$BLUE, $GREEN, $RED, $PURPLE, $ORANGE, $YELLOW];
 
+$CHOCOLATE = "chocolate";
+$COOOKIE = "cookie";
+$NUTS = "nuts";
+$PIE = "pie";
+$SPICES = "spices";
+$ICE_CREAM = "iceCream";
+$FRUIT = "fruit";
+$CAKE = "cake";
+$MARSHMALLOW = "marshmallow";
+$PASTRY = "pastry";
+$VEGGIES = "veggies";
+$PUDDING = "pudding";
+$this->flavours = [$CHOCOLATE, $COOOKIE, $NUTS, $PIE, $SPICES, $ICE_CREAM, $FRUIT, $CAKE, $MARSHMALLOW, $PASTRY, $VEGGIES, $PUDDING];
 
+$this->desserts = array(
+  1 => array(
+    'name' => clienttranslate('BLACK_FOREST_CAKE'),
+    'nametr' => self::_('blackForestCake'),
+    'taste1' => $CHOCOLATE,
+    'taste2' => $FRUIT,
+    'taste3' => $CAKE,
+    'taste4' => null,
+  ),
+  2 => array(
+    'name' => clienttranslate('BOSTON_CREAM_PIE'),
+    'nametr' => self::_('bostonCreamPie'),
+    'taste1' => $CAKE,
+    'taste2' => $PUDDING,
+    'taste3' => $PIE,
+    'taste4' => $CHOCOLATE,
+  ),
+  3 => array(
+    'name' => clienttranslate('BUTTER_PECAN_ICE_CREAM'),
+    'nametr' => self::_('butterPecanIceCream'),
+    'taste1' => $NUTS,
+    'taste2' => $ICE_CREAM,
+    'taste3' => null,
+    'taste4' => null,
+  ),
+  4 => array(
+    'name' => clienttranslate('CAKE_DONUT_WITH_SPRINKLES'),
+    'nametr' => self::_('cakeDonutWithSprinkles'),
+    'taste1' => $CAKE,
+    'taste2' => $PASTRY,
+    'taste3' => null,
+    'taste4' => null,
+  ),
+  5 => array(
+    'name' => clienttranslate('CANDIED_GINGER'),
+    'nametr' => self::_('candiedGinger'),
+    'taste1' => $SPICES,
+    'taste2' => null,
+    'taste3' => null,
+    'taste4' => null,
+  ),
+  6 => array(
+    'name' => clienttranslate('CANDIED_YAMS'),
+    'nametr' => self::_('candiedYams'),
+    'taste1' => $MARSHMALLOW,
+    'taste2' => $VEGGIES,
+    'taste3' => null,
+    'taste4' => null,
+  )
+);
 
-
+$this->guests = array(
+  1 => array(
+    'name' => clienttranslate('AGENT_17'),
+    'nametr' => self::_('agent17'),
+    'taste1' => $PASTRY,
+    'taste2' => $FRUIT,
+    'taste3' => null,
+    'taste4' => null,
+    'dislike' => $NUTS,
+    'color' => $RED,
+    'favourite1' => "APPLE_TURNOVER",
+    'favourite2' => "BELGIAN WAFFLE",
+  ),
+  2 => array(
+    'name' => clienttranslate('BOB_FRUITCAKE'),
+    'nametr' => self::_('bobFruitcake'),
+    'taste1' => $FRUIT,
+    'taste2' => $CAKE,
+    'taste3' => $NUTS,
+    'taste4' => null,
+    'dislike' => null,
+    'color' => $PURPLE,
+    'favourite1' => "FRUITCAKE",
+    'favourite2' => null,
+  ),
+  3 => array(
+    'name' => clienttranslate('BOSTON_GUY'),
+    'nametr' => self::_('bostonGuy'),
+    'taste1' => $CAKE,
+    'taste2' => $PUDDING,
+    'taste3' => $PIE,
+    'taste4' => $CHOCOLATE,
+    'dislike' => null,
+    'color' => $GREEN,
+    'favourite1' => "BOSTON_CREAM_PIE",
+    'favourite2' => null,
+  )
+);
